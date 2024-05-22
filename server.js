1beabb4d89a10e8829d8ae8041f781b9eb2e643c
Ace-kv <talhashahid100@yahoo.com>
2024-05-22 15:07:01 +0500

    created a simple RESTful API with non-custom routes i.e. same as in api.ts in the front-end for the app

---
+++ server.js
@@ -0,0 +1,14 @@
+import { create, router as _router, defaults } from "json-server";
+
+const server = create();
+const router = _router("todos.json");
+const middlewares = defaults();
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
+
+export default server;
